57c052707ef232314d38dadf9c20481a2794c3b2
F4R-00F <farhinyasarkhan@gmail.com>
2026-09-01 23:15:39 +0600

    Add basic-01: calculate average of three numbers

---
+++ basic-01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    float avg;
+
+    a = 150;
+    b = 175;
+    c = 210;
+
+    avg = (a + b + c) / 3.0;
+    printf("Average: %f\n", avg);
+
+    return 0;
+}
